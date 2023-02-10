8c8a8df6165c18c007c9f9b59c37b38939e76ff0
songchez <tama4840@gmail.com>
2023-02-10 22:00:08 +0900

    실패. 새로운프로젝트만들자

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,19 +4,9 @@ import Layout from "../components/Layout";
 import "../styles/globals.css";
 
 export default function MyApp({ Component, pageProps }) {
-  function kakaoInit() {
-    // 페이지가 로드되면 실행
-    window.Kakao.init(process.env.NEXT_PUBLIC_KAKAO_JS_KEY);
-    console.log(window.Kakao.isInitialized());
-  }
-
   return (
     <Layout>
       <Component {...pageProps} />
-      <Script
-        src="https://developers.kakao.com/sdk/js/kakao.js"
-        onLoad={kakaoInit}
-      ></Script>
     </Layout>
   );
 }
